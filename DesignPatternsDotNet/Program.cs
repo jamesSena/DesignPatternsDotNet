cc885ff21f493d0c998965cb147aea6a2230edd5
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-12-20 15:26:32 -0300

    first commit

---
+++ DesignPatternsDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
